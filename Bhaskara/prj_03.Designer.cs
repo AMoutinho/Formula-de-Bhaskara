717275ede828c551875b01546e9e66d6e8a8a5ad
André Moutinho <68257070+AMoutinho@users.noreply.github.com>
2023-06-09 23:18:55 -0300

    Inclusão dos arquivos do projeto

---
+++ Bhaskara/prj_03.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Bhaskara
+{
+    partial class prj_03
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(prj_03));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_v1 = new System.Windows.Forms.Label();
+            this.tbA = new System.Windows.Forms.TextBox();
+            this.tbB = new System.Windows.Forms.TextBox();
+            this.lbl_v2 = new System.Windows.Forms.Label();
+            this.tbC = new System.Windows.Forms.TextBox();
+            this.lbl_vl3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbR1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbR2 = new System.Windows.Forms.TextBox();
+            this.btn_sair = new System.Windows.Forms.Button();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.tbR3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Vineta BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fórmula de Bhaskara";
+            // 
+            // lbl_v1
+            // 
+            this.lbl_v1.AutoSize = true;
+            this.lbl_v1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_v1.Location = new System.Drawing.Point(31, 44);
+            this.lbl_v1.Name = "lbl_v1";
+            this.lbl_v1.Size = new System.Drawing.Size(81, 20);
+            this.lbl_v1.TabIndex = 2;
+            this.lbl_v1.Text = "Valor de a";
+            // 
+            // tbA
+            // 
+            this.tbA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbA.Location = new System.Drawing.Point(133, 41);
+            this.tbA.Name = "tbA";
+            this.tbA.Size = new System.Drawing.Size(86, 26);
+            this.tbA.TabIndex = 3;
+            this.tbA.Text = "0";
+            this.tbA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbA_KeyPress);
+            // 
+            // tbB
+            // 
+            this.tbB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbB.Location = new System.Drawing.Point(133, 73);
+            this.tbB.Name = "tbB";
+            this.tbB.Size = new System.Drawing.Size(86, 26);
+            this.tbB.TabIndex = 5;
+            this.tbB.Text = "0";
+            this.tbB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbB_KeyPress);
+            // 
+            // lbl_v2
+            // 
+            this.lbl_v2.AutoSize = true;
+            this.lbl_v2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_v2.Location = new System.Drawing.Point(31, 76);
+            this.lbl_v2.Name = "lbl_v2";
+            this.lbl_v2.Size = new System.Drawing.Size(81, 20);
+            this.lbl_v2.TabIndex = 4;
+            this.lbl_v2.Text = "Valor de b";
+            // 
+            // tbC
+            // 
+            this.tbC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbC.Location = new System.Drawing.Point(133, 105);
+            this.tbC.Name = "tbC";
+            this.tbC.Size = new System.Drawing.Size(86, 26);
+            this.tbC.TabIndex = 7;
+            this.tbC.Text = "0";
+            this.tbC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbC_KeyPress);
+            // 
+            // lbl_vl3
+            // 
+            this.lbl_vl3.AutoSize = true;
+            this.lbl_vl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_vl3.Location = new System.Drawing.Point(31, 108);
+            this.lbl_vl3.Name = "lbl_vl3";
+            this.lbl_vl3.Size = new System.Drawing.Size(80, 20);
+            this.lbl_vl3.TabIndex = 6;
+            this.lbl_vl3.Text = "Valor de c";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(35, 135);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(188, 27);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(63, 257);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "x\'";
+            // 
+            // tbR1
+            // 
+            this.tbR1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbR1.Location = new System.Drawing.Point(91, 254);
+            this.tbR1.Name = "tbR1";
+            this.tbR1.ReadOnly = true;
+            this.tbR1.Size = new System.Drawing.Size(86, 26);
+            this.tbR1.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(60, 289);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "x\'\'";
+            // 
+            // tbR2
+            // 
+            this.tbR2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbR2.Location = new System.Drawing.Point(91, 286);
+            this.tbR2.Name = "tbR2";
+            this.tbR2.ReadOnly = true;
+            this.tbR2.Size = new System.Drawing.Size(86, 26);
+            this.tbR2.TabIndex = 12;
+            // 
+            // btn_sair
+            // 
+            this.btn_sair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sair.Location = new System.Drawing.Point(35, 201);
+            this.btn_sair.Name = "btn_sair";
+            this.btn_sair.Size = new System.Drawing.Size(188, 27);
+            this.btn_sair.TabIndex = 13;
+            this.btn_sair.Text = "Sair";
+            this.btn_sair.UseVisualStyleBackColor = true;
+            this.btn_sair.Click += new System.EventHandler(this.btn_sair_Click);
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpar.Location = new System.Drawing.Point(35, 168);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(188, 27);
+            this.btn_limpar.TabIndex = 14;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // tbR3
+            // 
+            this.tbR3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbR3.Location = new System.Drawing.Point(152, 326);
+            this.tbR3.Name = "tbR3";
+            this.tbR3.ReadOnly = true;
+            this.tbR3.Size = new System.Drawing.Size(86, 26);
+            this.tbR3.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 329);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 20);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Resultado Delta :";
+            // 
+            // prj_03
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(250, 362);
+            this.ControlBox = false;
+            this.Controls.Add(this.tbR3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.btn_sair);
+            this.Controls.Add(this.tbR2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbR1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.tbC);
+            this.Controls.Add(this.lbl_vl3);
+            this.Controls.Add(this.tbB);
+            this.Controls.Add(this.lbl_v2);
+            this.Controls.Add(this.tbA);
+            this.Controls.Add(this.lbl_v1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "prj_03";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fórmula de Bhaskara";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_v1;
+        private System.Windows.Forms.TextBox tbA;
+        private System.Windows.Forms.TextBox tbB;
+        private System.Windows.Forms.Label lbl_v2;
+        private System.Windows.Forms.TextBox tbC;
+        private System.Windows.Forms.Label lbl_vl3;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbR1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbR2;
+        private System.Windows.Forms.Button btn_sair;
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.TextBox tbR3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Bhaskara/prj_03.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bhaskara
+{
+    public partial class prj_03 : Form
+    {
+        public prj_03()
+        {
+            InitializeComponent();
+        }
+        #region Calcular
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            #region Declaração de Variaveis
+            double a = 0, b = 0, c = 0, delta =0;
+            try
+            {
+               
+                a = double.Parse(tbA.Text);
+                b = double.Parse(tbB.Text);
+                c = double.Parse(tbC.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Digite os 3 valores.");
+            }
+            #endregion
+            #region Teste Lógico
+            if (a != 0)
+            {
+                
+                delta = Math.Pow(b, 2) - (4 * a * c);
+
+                if (delta >= 0)
+                {
+                    tbR1.Text = ((-b + Math.Sqrt(delta)) / (2 * a)).ToString("#.##");
+                    tbR2.Text = ((-b - Math.Sqrt(delta)) / (2 * a)).ToString("#.##");
+                    tbR3.Text = delta.ToString("#.##");
+                }
+                else
+                {
+                    MessageBox.Show("Delta não pode ser negativo. \n Δ = " + delta, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            else
+            {
+                MessageBox.Show("\"a\" não pode ser 0");
+            }
+        }
+            #endregion
+            #region KeyPress
+        private void tbA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            if ((!char.IsDigit(e.KeyChar)) && (e.KeyChar != 8) && (e.KeyChar != 45))
+            {
+                
+                e.Handled = true;
+            }
+        }
+
+        private void tbB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+            if ((!char.IsDigit(e.KeyChar)) && (e.KeyChar != 8) && (e.KeyChar != 45))
+            {
+                
+                e.Handled = true;
+            }
+        }
+
+        private void tbC_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+            if ((!char.IsDigit(e.KeyChar)) && (e.KeyChar != 8) && (e.KeyChar != 45))
+            {
+                
+                e.Handled = true;
+            }
+        #endregion
+        }
+#endregion
+        #region Limpar
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            tbA.Text = ("");
+            tbB.Text = ("");
+            tbC.Text = ("");
+            tbR1.Text = ("");
+            tbR2.Text = ("");  
+        }
+        #endregion
+        #region Sair
+        private void btn_sair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        #endregion
+    }
+}
